ea75e84a87e464c4e50673e922884ed5482c33d5
Srikar Sai <70573179+Srikar-ai@users.noreply.github.com>
2022-02-10 11:30:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{Component} from 'react';
+import axios from 'axios';
 
-function App() {
+class App extends Component  {
+  constructor(){
+    super();
+    this.state={advice:''}
+  }
+  componentDidMount(){
+    console.log("Component Mounted");
+    this.fetchthedata();
+    }
+  fetchthedata=()=>{
+    axios.get('	https://api.adviceslip.com/advice')
+    .then(data=>{
+      console.log(data.data.slip.advice);
+      this.setState({advice:data.data.slip.advice})
+    })
+    .catch(error=>console.log(error))
+  }
+
+
+
+  render(){
+    console.log('rendered');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className='card'>  
+            <h1>{this.state.advice}</h1>
+            <button onClick={this.fetchthedata}>Click me</button>
+        </div>
+    </div>  
   );
+  }
 }
 
 export default App;
